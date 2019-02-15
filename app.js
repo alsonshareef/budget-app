a7bd2000bef8ace84694a9497ccba6307e9e483b
Alson Shareef <alson.shareef@gmail.com>
2019-02-15 19:09:14 +0800

    Added functions to setup event listeners and to initialize the application, within appController

--- app.js
+++ app.js
@@ -34,26 +34,40 @@ const UIController = (function() {
 // MAIN APP CONTROLLER
 const appController = (function(budgetCtrl, UICtrl) {
 
-    // Stores local copy of DOMelements here using getDOMelement method of UIController
-    let DOM = UICtrl.getDOMelements()
+    // Setup event listeners
+        let setupEventListeners = () => {
+            // Stores local copy of DOMelements here using getDOMelement method of UIController
+                let DOM = UICtrl.getDOMelements()
+
+            // Event listeners for button and enter keypress to run 'addItem'
+                DOM.addButton.addEventListener('click', addItem)
+                document.addEventListener('keypress', (event) => {
+                    if (event.keyCode === 13 || event.which === 13) {
+                        addItem()
+                    }
+                })
+        }
 
     // Handles the input data
-    let addItem = () => {
-        // 1. Gather input data
-            let input = UICtrl.getInput()
-            console.log(input)
-        // 2. Add item to budget controller
-        // 3. Add item to UI controller
-        // 4. Calculate the budget
-        // 5. Display budget on the UI
-    }
-
-    // Event listeners for button and enter keypress to run 'addItem'
-    DOM.addButton.addEventListener('click', addItem)
-    document.addEventListener('keypress', (event) => {
-        if (event.keyCode === 13 || event.which === 13) {
-            addItem()
+        let addItem = () => {
+            // 1. Gather input data
+                let input = UICtrl.getInput()
+                console.log(input)
+            // 2. Add item to budget controller
+            // 3. Add item to UI controller
+            // 4. Calculate the budget
+            // 5. Display budget on the UI
         }
-    })
+
+    // Initialization
+        return {
+            init: function() {
+                console.log('App started!')
+                setupEventListeners()
+            }
+        }
 
 })(budgetController, UIController)
+
+// Starts application
+    appController.init()
