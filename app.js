0a4676768397b787f1c6b1acb8c8061455c27be8
Alson Shareef <alson.shareef@gmail.com>
2019-02-15 18:52:37 +0800

    Added UIController methods for getting input values from UI and sending DOMelements object to other controllers

--- app.js
+++ app.js
@@ -8,26 +8,48 @@ const budgetController = (function() {
 // UI CONTROLLER
 const UIController = (function() {
 
+    let DOMelements = {
+        inputType: document.querySelector('.add__type'),
+        inputDescription: document.querySelector('.add__description'),
+        inputValue: document.querySelector('.add__value'),
+        addButton: document.querySelector('.add__btn')
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: DOMelements.inputType.value, // Will look at the value attribute, not the property
+                description: DOMelements.inputDescription.value,
+                value: DOMelements.inputValue.value
+            }
+        },
+        getDOMelements: function() {
+            return DOMelements
+        }
+    }
+
 })()
 
 
 // MAIN APP CONTROLLER
 const appController = (function(budgetCtrl, UICtrl) {
 
+    // Stores local copy of DOMelements here using getDOMelement method of UIController
+    let DOM = UICtrl.getDOMelements()
+
     // Handles the input data
     let addItem = () => {
-        /*
-            1. Gather input data
-            2. Add item to budget controller
-            3. Add item to UI controller
-            4. Calculate the budget
-            5. Display budget on the UI
-        */
+        // 1. Gather input data
+            let input = UICtrl.getInput()
+            console.log(input)
+        // 2. Add item to budget controller
+        // 3. Add item to UI controller
+        // 4. Calculate the budget
+        // 5. Display budget on the UI
     }
 
-
     // Event listeners for button and enter keypress to run 'addItem'
-    document.querySelector('.add__btn').addEventListener('click', addItem)
+    DOM.addButton.addEventListener('click', addItem)
     document.addEventListener('keypress', (event) => {
         if (event.keyCode === 13 || event.which === 13) {
             addItem()
