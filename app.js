e675707204769d6366b31e4ebca9dd33d8d926a3
Alson Shareef <alson.shareef@gmail.com>
2019-02-18 18:23:48 +0800

    Added a data object to store items, and an addItem method to store new items into the data object within budget controller

--- app.js
+++ app.js
@@ -14,6 +14,43 @@ const budgetController = (function() {
         this.value = value
     }
 
+    let data = {
+        allItems: {
+            income: [],
+            expense: []
+        },
+        totals: {
+            income: 0,
+            expense: 0
+        }
+    }
+
+    return {
+        addItem: function(type, desc, val) {
+            let newItem, id;
+
+            // Generates new ID based on id of last item in 'income' or 'expense' array
+            if (data.allItems[type].length > 0) {
+                id = data.allItems[type][data.allItems[type].length - 1].id + 1
+            } else {
+                id = 0
+            }
+
+            // Generates new income or expense object
+            if (type === 'expense') {
+                newItem = new Expense(id, desc, val)
+            } else if (type === 'income') {
+                newItem = new Income(id, desc, val)
+            }
+
+            // Adds the newItem to its respective array in data structure
+            data.allItems[type].push(newItem)
+
+            // Returns the newItem to be used by other controllers
+            return newItem
+        }
+    }
+
 })()
 
 
@@ -30,7 +67,7 @@ const UIController = (function() {
     return {
         getInput: function() {
             return {
-                type: DOMelements.inputType.value, // Will look at the value attribute, not the property
+                type: DOMelements.inputType.value, // Will look at the value attribute (income or expense), not the property
                 description: DOMelements.inputDescription.value,
                 value: DOMelements.inputValue.value
             }
@@ -62,10 +99,14 @@ const appController = (function(budgetCtrl, UICtrl) {
 
     // Handles the input data
         let addItem = () => {
+            let input, newItem
+
             // 1. Gather input data
-                let input = UICtrl.getInput()
-                console.log(input)
+                input = UICtrl.getInput()
+
             // 2. Add item to budget controller
+                newItem = budgetCtrl.addItem(input.type, input.description, input.value)
+
             // 3. Add item to UI controller
             // 4. Calculate the budget
             // 5. Display budget on the UI
